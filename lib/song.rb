adca7e6920c743f5f08b5bebedf5b78250f1cf1f
Jae Yong Kim <kim900180@gmail.com>
2018-08-08 20:18:44 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name, artist)
+    @name = name
+    @artist = artist
+    @@all << self
+  end
+
+  def artist_name
+    self.artist.name
+  end
+
+  def self.all
+    @@all
+  end
+end
