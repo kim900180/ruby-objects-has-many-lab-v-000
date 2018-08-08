f2b42a669ccec9e0405572f7595d590ea7fa0d47
Jae Yong Kim <kim900180@gmail.com>
2018-08-08 20:37:18 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,6 @@
 class Artist
   attr_accessor :name
 
-  # @@song_count = 0
-
   def initialize(name)
     @name = name
     @songs = []
@@ -11,14 +9,12 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    # @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
-    # @@song_count += 1
   end
 
   def songs
